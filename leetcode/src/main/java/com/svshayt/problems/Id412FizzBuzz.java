ed14ca88e1b6dd6b05ab4c7c6c9d7f7c34a4389f
Vasily_Shaytan <svshayt@gmail.com>
2023-03-12 17:33:03 +0300

    Add leetcode solution (412. Fizz Buzz)

--- leetcode/src/main/java/com/svshayt/problems/Id412FizzBuzz.java
+++ leetcode/src/main/java/com/svshayt/problems/Id412FizzBuzz.java
@@ -6,10 +6,10 @@ import java.util.List;
 
 /**
  * 412. Fizz Buzz
- * #Easy
- *
- * <a href="https://leetcode.com/problems/fizz-buzz/">...</a>
- *
+ * Easy
+ * 1.7K
+ * 249
+ * Companies
  * Given an integer n, return a string array answer (1-indexed) where:
  *
  * answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
@@ -17,18 +17,15 @@ import java.util.List;
  * answer[i] == "Buzz" if i is divisible by 5.
  * answer[i] == i (as a string) if none of the above conditions are true.
  *
+ *
  * Example 1:
  *
  * Input: n = 3
  * Output: ["1","2","Fizz"]
- *
- *
  * Example 2:
  *
  * Input: n = 5
  * Output: ["1","2","Fizz","4","Buzz"]
- *
- *
  * Example 3:
  *
  * Input: n = 15
@@ -38,11 +35,22 @@ import java.util.List;
  * Constraints:
  *
  * 1 <= n <= 104
- *
- * Related Topics:
- * #Math
- * #String
- * #Simulation
+ * Accepted
+ * 914.3K
+ * Submissions
+ * 1.3M
+ * Acceptance Rate
+ * 69.8%
+ * Seen this question in a real interview before?
+ * 1/4
+ * Yes
+ * No
+ * Discussion (32)
+ * Similar Questions
+ * Related Topics
+ * Math
+ * String
+ * Simulation
  */
 public class Id412FizzBuzz {
     public List<String> fizzBuzz(int n) {

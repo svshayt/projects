b7525f0fd6f57e0d2806e36074f642194abe1e80
Vasily_Shaytan <svshayt@gmail.com>
2023-04-24 17:35:57 +0300

    Add case with @Value annotation

--- spring-boot-up-and-running-maven/src/main/java/com/svshayt/GreetingController.java
+++ spring-boot-up-and-running-maven/src/main/java/com/svshayt/GreetingController.java
@@ -13,8 +13,16 @@ public class GreetingController {
     @Value("${greeting-name: Mirage}")
     private String name;
 
+    @Value("${greeting-coffee: ${greeting-name} is drinking Cafe Ganador}")
+    private String coffee;
+
     @GetMapping
     String getGreeting() {
         return name;
+    }
+
+    @GetMapping("/coffee")
+    String getNameAndCoffee() {
+        return coffee;
     }
 }

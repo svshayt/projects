c88f33accb003857124d0dd96a5ffe4e45853da4
Vasily_Shaytan <svshayt@gmail.com>
2023-04-25 17:12:24 +0300

    Add refactoring code and classes

--- spring-boot-up-and-running-maven/src/main/java/com/svshayt/Application.java
+++ spring-boot-up-and-running-maven/src/main/java/com/svshayt/Application.java
@@ -2,22 +2,9 @@ package com.svshayt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import java.util.Optional;
-import java.util.UUID;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
@@ -25,78 +12,10 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-}
 
-@Entity
-class Coffee {
-
-    @Id
-    private String id;
-    private String name;
-
-    public Coffee() {
-
-    }
-
-    public Coffee(String id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public Coffee(String name) {
-        this(UUID.randomUUID().toString(), name);
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-}
-
-@RestController
-@RequestMapping("/coffees")
-class RestApiController {
-    private final CoffeeRepository coffeeRepository;
-
-    public RestApiController(CoffeeRepository coffeeRepository) {
-        this.coffeeRepository = coffeeRepository;
-    }
-
-    @GetMapping
-    Iterable<Coffee> getCoffees() {
-        return coffeeRepository.findAll();
-    }
-
-    @GetMapping("/{id}")
-    Optional<Coffee> getCoffeeById(@PathVariable String id) {
-        return coffeeRepository.findById(id);
-    }
-
-    @PostMapping
-    Coffee postCoffee(@RequestBody Coffee coffee) {
-        return coffeeRepository.save(coffee);
-    }
-
-    @PutMapping("/{id}")
-    ResponseEntity<Coffee> putCoffee(@PathVariable String id, @RequestBody Coffee coffee) {
-        return (coffeeRepository.existsById(id))
-                ? new ResponseEntity<>(coffeeRepository.save(coffee), HttpStatus.OK)
-                : new ResponseEntity<>(coffeeRepository.save(coffee), HttpStatus.CREATED);
-    }
-
-    @DeleteMapping("/{id}")
-    void deleteCoffee(@PathVariable String id) {
-        coffeeRepository.deleteById(id);
+    @Bean
+    @ConfigurationProperties(prefix = "droid")
+    Droid createDroid() {
+        return new Droid();
     }
 }

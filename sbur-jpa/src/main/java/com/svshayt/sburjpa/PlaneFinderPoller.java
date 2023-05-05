5b0613e498cc560314c49cc071cb6a6c9a8e1c39
Vasily_Shaytan <svshayt@gmail.com>
2023-05-05 11:19:49 +0300

    Add sbur-jpa project

---
+++ sbur-jpa/src/main/java/com/svshayt/sburjpa/PlaneFinderPoller.java
@@ -0,0 +1,31 @@
+package com.svshayt.sburjpa;
+
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@EnableScheduling
+@Component
+@RequiredArgsConstructor
+public class PlaneFinderPoller {
+    @NonNull
+    private final AircraftRepository aircraftRepository;
+    private final WebClient webClient = WebClient.create("http://localhost:7634/aircraft");
+
+    @Scheduled(fixedRate = 1000)
+    private void pollPlanes() {
+        aircraftRepository.deleteAll();
+
+        webClient.get()
+                .retrieve()
+                .bodyToFlux(Aircraft.class)
+                .filter(plane -> !plane.getReg().isEmpty())
+                .toStream()
+                .forEach(aircraftRepository::save);
+
+        aircraftRepository.findAll().forEach(System.out::println);
+    }
+}
---
+++ sbur-jpa/src/main/java/com/svshayt/sburjpa/SburJpaApplication.java
@@ -0,0 +1,13 @@
+package com.svshayt.sburjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SburJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SburJpaApplication.class, args);
+    }
+
+}
